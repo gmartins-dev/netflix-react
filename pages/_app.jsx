e1946f0e31ac66046b433816f5ec1f8a67772bf2
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-15 10:55:37 -0300

    add title on head page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 //Importar tudo que for global para essa pagina
 import '../src/globalStyle.css'
+import Head from 'next/head'
 
 function App ({Component, pageProps}) {
 
     return(
-
+        <>
+        <Head><title>Netflix Clone using React/Next</title></Head>
         <Component {...pageProps} />
+        </>
     )
 
 }
